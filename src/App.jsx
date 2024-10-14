fe5c82a9c4fd2b3b6d342e211376523baa4e0492
mani0bne <manihasnetha@gmail.com>
2024-10-14 10:55:50 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Sidebar from "./components/Sidebar"
+import MainContent from "./components/MainContent"
+
+
+const App = () => {
+  return (
+    <>
+      <div className="flex animate-fadeIn duration-1000">
+        <Sidebar />
+        <MainContent />
+      </div>
+    </>
+  )
+}
+
+export default App
